5bb398a700796100614dccc2878645f06c22f74d
David <ekechukwudavid@gmail.com>
2023-05-02 18:22:38 +0530

    setup the serializer for the projects

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,5 +1,15 @@
-from django.urls import path
+from django.db import router
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
 from . import views
+
+router = DefaultRouter()
+router.register(r'tables', views.BookingViewSet)
+
 urlpatterns =[
     path('', views.index, name='index'),
+    path('menu/', views.MenuItemView.as_view(), name='menu-view'),
+    path('menu/<int:pk>', views.SingleMenuItemView.as_view(), name='single-menu-view'),
+    path('restaurant/booking/', include(router.urls)),
 ]
