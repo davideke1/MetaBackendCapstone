34a270ce7898c04538d141c0fa1ce3305758dfb0
David <ekechukwudavid@gmail.com>
2023-05-05 22:54:07 +0530

    added test for views

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -3,5 +3,8 @@ from django.contrib import admin
 # Register your models here.
 from .models import Menu, Boking
 
-admin.site.register(Menu)
+class MenuAdmin(admin.ModelAdmin):
+    list_display = ('title', 'get_item', 'inventory')
+
+admin.site.register(Menu, MenuAdmin)
 admin.site.register(Boking)
