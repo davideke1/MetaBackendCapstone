030519a51ef992edf63ba8ede19c15bc87f4eefa
David <ekechukwudavid@gmail.com>
2023-06-18 10:20:22 +0530

    added test for vercel

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -78,17 +78,26 @@ WSGI_APPLICATION = 'littlelemon.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME' : 'littlelemon',
+#         'HOST': '127.0.0.1',
+#         'PORT': '3306',
+#         'USER': 'root',
+#         'PASSWORD': 'Daviduzoma_1',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME' : 'littlelemon',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'restoreserve1',
+        'USER': 'postgres',
+        'PASSWORD': 'david',
         'HOST': '127.0.0.1',
-        'PORT': '3306',
-        'USER': 'root',
-        'PASSWORD': 'Daviduzoma_1',
+        'PORT': '5433',
     }
 }
-
 
 
 # Password validation
